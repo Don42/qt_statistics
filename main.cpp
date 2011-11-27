fb93fccad0f9212e161c5f858d8f5957ae07a9df
Don42 <DonMarco42@gmail.com>
2011-11-27 13:16:08 +0100

    Further cleanup and 2 Plot Layout

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <QtGui/QApplication>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
 #include <qwt_plot_histogram.h>
 #include <qwt_series_data.h>
 #include <qwt_global.h>
 #include <qwt_plot.h>
+#include <qwt_column_symbol.h>
+
 #include <stdio.h>
 #include <iostream>
-#include <qwt_column_symbol.h>
-#include <qpen.h>
+
 #include "experiment.h"
 
 int main( int argc, char *argv[] )
@@ -18,51 +22,72 @@ int main( int argc, char *argv[] )
     int sumStay = 0;
 
     QApplication a (argc, argv);
-    QwtPlot w;
+    QWidget *window = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout();
+    QwtPlot pSwitch, pStay;
     Experiment e;
+    
+    layout->addWidget(&pSwitch);
+    layout->addWidget(&pStay);
 
-    w.setTitle( "Wins/Loses" );
+    window->setLayout(layout);
+
+    pSwitch.setTitle( "Allways Switch" );
+    pStay.setTitle( "Allways Stay" );
+    
+    
 
     //Setup Histogram Plot
-    QwtPlotHistogram *h = new QwtPlotHistogram();
+    QwtPlotHistogram *hSwitch = new QwtPlotHistogram( "allwaysSwitch" );
+    QwtPlotHistogram *hStay = new QwtPlotHistogram( "allwaysStay" );
     QwtColumnSymbol *symbol = new QwtColumnSymbol( QwtColumnSymbol::Box );
     symbol->setFrameStyle( QwtColumnSymbol::Raised );
     symbol->setPalette(Qt::darkCyan);
-    h->setSymbol(symbol);
-    h->setBaseline(0);
+    hSwitch->setSymbol(symbol);
+    hStay->setSymbol(symbol);
+    hSwitch->setBaseline(0);
+    hStay->setBaseline(0);
 
 
     for(n = 0; n<m;n++)
     {
-        int curResult = e.conductExperiment(Experiment::allwaysStay);
-        sumStay += curResult;
-
-        curResult = e.conductExperiment(Experiment::allwaysSwitch);
+        int curResult = e.conductExperiment(Experiment::allwaysSwitch);
         sumSwitch += curResult;
+
+        curResult = e.conductExperiment(Experiment::allwaysStay);
+        sumStay += curResult;
 
         if((n%1000)==0)
         {
-            QVector<QwtIntervalSample> data;
-            data.append( QwtIntervalSample(n,0,1));
-            data.append( QwtIntervalSample(sumSwitch,1,2));
-            data.append( QwtIntervalSample(n-sumSwitch,2,3));
-            data.append( QwtIntervalSample(n,5,6));
-            data.append( QwtIntervalSample(sumStay,6,7));
-            data.append( QwtIntervalSample(n-sumStay,7,8));
+            QVector<QwtIntervalSample> dataSwitch;
+            dataSwitch.append( QwtIntervalSample(n,0,1));
+            dataSwitch.append( QwtIntervalSample(sumSwitch,1,2));
+            dataSwitch.append( QwtIntervalSample(n-sumSwitch,2,3));
+
+            QVector<QwtIntervalSample> dataStay;
+            dataStay.append( QwtIntervalSample(n,5,6));
+            dataStay.append( QwtIntervalSample(sumStay,6,7));
+            dataStay.append( QwtIntervalSample(n-sumStay,7,8));
             
-            h->setSamples(data);
-            h->attach(&w);
-            w.replot();
-            w.show();
+            hSwitch->setSamples(dataSwitch);
+            hStay->setSamples(dataStay);
+            hSwitch->attach(&pSwitch);
+            hStay->attach(&pStay);
+            pSwitch.replot();
+            pStay.replot();
+            window->show();
             std::cout << "\r";
             std::cout << (n/(double)m)*100 << "%";
         }
     }
-    std::cout << "Number of Wins: " << sumStay << std::endl;
-    std::cout << "Number of Loses: " << m-sumStay << std::endl;
-
+    std::cout << "\r";
+    std::cout << "Allways Switching" << std::endl;
     std::cout << "Number of Wins: " << sumSwitch << std::endl;
     std::cout << "Number of Loses: " << m-sumSwitch << std::endl;
+
+    std::cout << "Allways Staying" << std::endl;
+    std::cout << "Number of Wins: " << sumStay << std::endl;
+    std::cout << "Number of Loses: " << m-sumStay << std::endl;
 
 
 
