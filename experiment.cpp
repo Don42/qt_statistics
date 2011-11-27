fdee96b18e9ea0bd2b35f79df99c2dd195c9c145
Don42 <DonMarco42@gmail.com>
2011-11-27 01:36:35 +0100

    Implemented visualisation and running 5000 tests each Visualisation with QwtPlotHistogram

--- experiment.cpp
+++ experiment.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -28,12 +26,6 @@ char Experiment::conductExperiment(char switchingMode)
         openDoor = rand() % 3;
     } while(openDoor==winningDoor||openDoor==choosenDoor);
     
-    using namespace std;
-    cout << "winningDoor: " << (int)winningDoor << endl;
-    cout << "choosenDoor: " << (int)choosenDoor << endl;
-    //cout << "switchingDoor: " << (int)switchingDoor << endl;
-    cout << "openDoor: " << (int)openDoor << endl;
-    //cout << "remainingDoor: " << (int)remainingDoor << endl;
     if(switchingMode == allwaysStay)
     {
         if(choosenDoor == winningDoor)
--- main.cpp
+++ main.cpp
@@ -1,23 +1,67 @@
 #include <QtGui/QApplication>
+#include <qwt_plot_histogram.h>
+#include <qwt_series_data.h>
+#include <qwt_global.h>
+#include <qwt_plot.h>
 #include <stdio.h>
 #include <iostream>
-
+#include <qwt_column_symbol.h>
+#include <qpen.h>
 #include "experiment.h"
 
 int main( int argc, char *argv[] )
 {
+
     int n, m;
     m = 5000;
-    int sum = 0;
+    int sumSwitch = 0;
+    int sumStay = 0;
+
     QApplication a (argc, argv);
-    
+    QwtPlot w;
     Experiment e;
+
+    w.setTitle( "Wins/Loses" );
+
+    //Setup Histogram Plot
+    QwtPlotHistogram *h = new QwtPlotHistogram();
+    QwtColumnSymbol *symbol = new QwtColumnSymbol( QwtColumnSymbol::Box );
+    symbol->setFrameStyle( QwtColumnSymbol::Raised );
+    symbol->setPalette(Qt::darkCyan);
+    h->setSymbol(symbol);
+    h->setBaseline(0);
+
+
+    for(n = 0; n<m;n++)
+    {
+        int curResult = e.conductExperiment(Experiment::allwaysStay);
+        //std::cout << "Result: " << curResult << std::endl << std::endl;
+        sumStay += curResult;
+    }
+    std::cout << "Number of Wins: " << sumStay << std::endl;
+    std::cout << "Number of Loses: " << m-sumStay << std::endl;
+
     for(n = 0; n<m;n++)
     {
         int curResult = e.conductExperiment(Experiment::allwaysSwitch);
-        std::cout << "Result: " << curResult << std::endl << std::endl;
-        sum += curResult;
+        //std::cout << "Result: " << curResult << std::endl << std::endl;
+        sumSwitch += curResult;
     }
-    std::cout << "Number of Wins: " << sum << std::endl;
+    std::cout << "Number of Wins: " << sumSwitch << std::endl;
+    std::cout << "Number of Loses: " << m-sumSwitch << std::endl;
 
+
+    QVector<QwtIntervalSample> data;
+    data.append( QwtIntervalSample(sumSwitch,1,2));
+    data.append( QwtIntervalSample(m-sumSwitch,2,3));
+    data.append( QwtIntervalSample(sumStay,5,6));
+    data.append( QwtIntervalSample(m-sumStay,6,7));
+
+    h->setSamples(data);
+    h->attach(&w);
+
+
+    w.replot();
+    w.show();
+    return a.exec();
 }
